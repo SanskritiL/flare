2ded6b03b8130deff113b114285ad910697409cd
Sanskriti Lamsal <slamsal@go.olemiss.edu>
2020-01-22 23:13:34 -0600

    inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(home: MyHome(),));
 
-void main()=>runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Unit FounTEEN', style: TextStyle(fontFamily: 'Handlee', color: Colors.black, fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.orange[400],       
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          color: Colors.orange[400],
+          child: Container(
+            height: 50
+          ),
+        ),
+         floatingActionButton: FloatingActionButton(
+           splashColor: Colors.orange[400],
+           onPressed: () => print("hello"),
+           child: Text('Next', style: TextStyle(fontFamily: 'Handlee', color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),), backgroundColor: Colors.white, 
+         ),
+         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+           
+  );
+
   }
 }
